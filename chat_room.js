eed59604a8797f4fd15d55d67f62d32c4018d775
kishaanm <106635051+kishaanm@users.noreply.github.com>
2022-10-24 18:48:38 -0400

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyATRtvzULjQa7WzeJpsNP6pSFx72gYdsFc",
+    authDomain: "let-s-chat-8441d.firebaseapp.com",
+    databaseURL: "https://let-s-chat-8441d-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-8441d",
+    storageBucket: "let-s-chat-8441d.appspot.com",
+    messagingSenderId: "670183050632",
+    appId: "1:670183050632:web:b6b441d77c44334433be3d"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
